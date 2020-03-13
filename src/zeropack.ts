54703f5bd92c82bcdb1c4464dcb46e604e9c0ee2
arzyu <arzyu@live.cn>
2020-03-13 21:19:02 +0800

    install webpack-cli, webpack-dev-server at runtime

--- src/zeropack.ts
+++ src/zeropack.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
+import fs, { accessSync } from "fs";
 import { resolve } from "path";
 import child_process from "child_process";
 
 import program from "commander-with-unknown-option-patched";
+import { getPackageBin } from "get-package-bin";
 import { getJson } from "@arzyu/get-json";
 
 program
@@ -13,12 +15,37 @@ program
   .allowUnknownOption(true)
   .parse(process.argv);
 
+const pkg = program.server ? "webpack-dev-server" : "webpack-cli";
+const pkgPath = resolve(process.cwd(), "./node_modules", pkg);
+
+try {
+  accessSync(resolve(pkgPath, "package.json"), fs.constants.R_OK);
+} catch (error) {
+  if (error.code === "ENOENT") {
+    const dependencies = {
+      "webpack-cli": ["webpack", "webpack-cli"],
+      "webpack-dev-server": ["webpack", "webpack-cli", "webpack-dev-server"]
+    };
+    const installCommand = `npm add ${dependencies[pkg].join(" ")} --no-save --no-save-dev`;
+
+    console.warn(installCommand);
+    child_process.execSync(installCommand, { stdio: "inherit" });
+
+  } else {
+    throw error;
+  }
+}
+
 const debugFlag = process.env.DEBUG === "true" ? "--inspect-brk" : "";
 const hook = program.server ? "./hook" : "./hook-webpack-cli";
-const pkg = program.server ? "webpack-dev-server/bin/webpack-dev-server" : "webpack-cli";
+const binFile = getPackageBin(pkgPath, pkg);
+
+if (!binFile) throw new Error(`${pkg} Not Found!`)
+
+const cmd = `node ${debugFlag} -r ${require.resolve(hook)} ${binFile}`;
 
 child_process.spawn(
-  `node ${debugFlag} -r ${require.resolve(hook)} ${require.resolve(pkg)}`,
+  cmd,
   program.unknownOptions,
   {
     env: {
