324261392e85ed59c383eafd3b7a497ed442a225
arzyu <arzyu@live.cn>
2021-10-19 19:07:52 +0800

    add more unadapted message

--- src/patch.webpack-cli.ts
+++ src/patch.webpack-cli.ts
@@ -6,6 +6,11 @@ import { getMatchedFiles, getMatchedHashes, getMatchedTargets, isFutureVersion }
 import { targetPatch, getTargets, getTargetsHash } from "./adapter";
 
 export const patch = (code: string, filename: string) => {
+  const printUnsupportedMessage = () => {
+    console.error(chalk`[airpack]: {gray Did you use the latest version of airpack? If so, please go to}`);
+    console.error(chalk`[airpack]: {gray https://github.com/arzyu/airpack/issues to report this error.}`);
+  };
+
   const wpc = process.env.AIRPACK_WPC;
   const wpcVersion = process.env.AIRPACK_WPC_VERSION!;
   const files = getMatchedFiles(wpcVersion);
@@ -15,6 +20,7 @@ export const patch = (code: string, filename: string) => {
 
   if (!target) {
     console.error(chalk`[airpack]: {red No targets defined in specs for file "${filename}"}`);
+    printUnsupportedMessage();
     process.exit()
   }
 
@@ -23,6 +29,7 @@ export const patch = (code: string, filename: string) => {
 
   if (!pTargets.length) {
     console.error(chalk`[airpack]: {red No targets matched to the target "${target}"}`);
+    printUnsupportedMessage();
     process.exit()
   }
 
@@ -38,8 +45,7 @@ export const patch = (code: string, filename: string) => {
   if (pTargetsHash !== hash) {
     if (isFutureVersion(wpcVersion)) {
       console.error(chalk`[airpack]: {red Target "${target}" has changed since the version of "${wpc}@${wpcVersion}"}`);
-      console.error(chalk`[airpack]: {gray Did you use the latest version of airpack? If so, please go to}`);
-      console.error(chalk`[airpack]: {gray https://github.com/arzyu/airpack/issues to report this error.}`);
+      printUnsupportedMessage();
       process.exit()
     }
     console.error(chalk`[airpack]: {red Hash not matched to the target "${target}"}`);
